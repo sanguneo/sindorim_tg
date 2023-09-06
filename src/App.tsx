f42a788eaa974e78cbe638a6688b9ccf59929354
sanguneo <sanguneo@acon3d.com>
2023-09-06 22:33:34 +0900

    nojekyll

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { GlobalStyles } from '@/shared/styles';
 import React from 'react';
 
 const App = (): React.ReactElement => {
-  const env = process.env.NODE_ENV;
   return (<>
       <Global styles={GlobalStyles} />
       <CustomRouter />
